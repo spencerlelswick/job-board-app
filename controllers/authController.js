049b55d22a46063981e868ec123dcfeb1be93cdf
spencerlelswick <spencerlelswick@gmail.com>
2023-05-24 15:36:48 -0400

    Added JWT and bcrypt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,30 @@
 import { StatusCodes } from 'http-status-codes'
 import User from '../models/User.js'
+import { BadRequestError } from '../errors/index.js'
 
 const register = async (req, res, next) => {
-  const user = await User.create(req.body)
-  res.status(StatusCodes.OK).json({ user })
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values')
+  }
+
+  const userAlreadyExists = await User.findOne({ email })
+  if (userAlreadyExists) throw new BadRequestError('Email already in use')
+
+  const user = await User.create({ name, email, password })
+  const token = user.createJWT()
+  res
+    .status(StatusCodes.OK)
+    .json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+      },
+      token,
+    })
 }
 const login = async (req, res) => {
   res.send('login user')
